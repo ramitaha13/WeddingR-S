ba8d4d18394c1988131695caf517f8b448a6b81c
WEBHM2-MAIN1-MAIN 2 <taharami14@gmail.com>
2024-11-24 10:04:01 +0200

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "/index.html",
+      },
+    },
   },
   plugins: [react()],
 });
