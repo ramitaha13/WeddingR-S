dd13774585dddcdab0c09a7101fc451feb2c7059
WEBHM2-MAIN1-MAIN 2 <taharami14@gmail.com>
2024-11-25 17:49:28 +0200

    Fix build configuration

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -62,7 +62,8 @@ const Checkout = () => {
 
       <PayPalScriptProvider
         options={{
-          "client-id": process.env.PAYPAL_CLIENT_ID,
+          "client-id":
+            "AY5h2PuwwKf8iR6wlmJt4a66enVIIS6WnOi6S5-m2RW7xdL3J9nnUIeAL70RaJS_nr402kIN35wwyJVc",
           currency: "ILS",
         }}
       >
